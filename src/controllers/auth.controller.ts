76fc2b2732252300765fa80ac22fc053b02a1147
ghazianshari <ghazi.anshari@gmail.com>
2025-09-04 14:42:16 +0700

    chore: something wrong with previous merging, regenerated prisma

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -3,7 +3,6 @@ import { registerUserService } from "../services/auth.service";
 import { validateRegisterUser } from "../services/functions/authValidation";
 
 export const registerUserController = async (req: Request, res: Response) => {
-    validateRegisterUser(req.body);
     const result = await registerUserService(req.body);
     res.status(200).json(result);
 };
