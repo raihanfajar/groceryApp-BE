3a35bd6c675267e26e539cb082b01b5f7a720936
ghazianshari <ghazi.anshari@gmail.com>
2025-09-23 19:42:16 +0700

    feat(location): add new address, display address, from navbar

--- src/services/geocoding.service.ts
+++ src/services/geocoding.service.ts
@@ -1,5 +1,4 @@
 import prisma from "../config";
-import { getLocationIdsFromNominatim } from "../helper/getLocationIdsFromNominatim";
 import { ApiError } from "../utils/ApiError";
 
 
@@ -38,18 +37,26 @@ interface IaddNewUserAddress {
     lat: number;
     lon: number;
     isDefault: boolean;
+    provinceId: number;
+    cityId: number;
+    districtId: number;
 }
 
 export const addNewUserAddressService = async (body: IaddNewUserAddress, userId: string) => {
-    const { addressLabel, receiverName, receiverPhoneNumber, addressDetails, lat, lon, isDefault, } = body;
-    // TODO: PROVINCE, CITY, DISTRICT --- CHECK RAJAONGKIR API
+    const { addressLabel, receiverName, receiverPhoneNumber, addressDetails, lat, lon, isDefault, provinceId = 1, cityId = 1, districtId = 1 } = body;
 
     const rgcResponse = await rgcService(lat.toString(), lon.toString()).then((res) => res);
-
 
     // !Extra validation
     const existingAddress = await prisma.userAddress.findFirst({ where: { userId, addressLabel } });
     if (existingAddress) throw new ApiError(409, "Address label already in use");
+    // !If isDefault is true, set all other address to false first
+    if (isDefault) {
+        await prisma.userAddress.updateMany({
+            where: { userId },
+            data: { isDefault: false },
+        });
+    }
 
     // !Determining address level
     // Province level
@@ -57,7 +64,7 @@ export const addNewUserAddressService = async (body: IaddNewUserAddress, userId:
         rgcResponse?.address?.state ??
         rgcResponse?.address?.region ??
         rgcResponse?.address?.province ??
-        rgcResponse?.address?.county ?? "This precise location has no province";
+        rgcResponse?.address?.county ?? "This precise location has no province from nominatim";
 
     // City level
     const cityLevel =
@@ -65,23 +72,19 @@ export const addNewUserAddressService = async (body: IaddNewUserAddress, userId:
         rgcResponse?.address?.town ??
         rgcResponse?.address?.municipality ??
         rgcResponse?.address?.village ??
-        "This precise location has no city";
+        "This precise location has no city from nominatim";
 
     // District / Subdistrict level
     const districtLevel =
         rgcResponse?.address?.city_district ??
         rgcResponse?.address?.suburb ??
         rgcResponse?.address?.neighbourhood ??
-        "This precise location has no district";
+        "This precise location has no district from nominatim";
 
     console.log(rgcResponse);
     console.log(provinceLevel?.toUpperCase());
     console.log(cityLevel?.toUpperCase());
     console.log(districtLevel?.toUpperCase());
-
-    // !Get location ids
-    const { provinceId, cityId, districtId } = await getLocationIdsFromNominatim(provinceLevel, cityLevel, districtLevel);
-    console.log(provinceId, cityId, districtId);
 
     //  !Add new address
     const newAddress = await prisma.userAddress.create({
@@ -96,11 +99,11 @@ export const addNewUserAddressService = async (body: IaddNewUserAddress, userId:
             lon,
             isDefault,
             province: provinceLevel?.toUpperCase(),
-            provinceId: provinceId ?? 1,
+            provinceId,
             city: cityLevel?.toUpperCase(),
-            cityId: cityId ?? 1,
+            cityId,
             district: districtLevel?.toUpperCase(),
-            districtId: districtId ?? 1,
+            districtId,
         },
     })
 
@@ -109,8 +112,66 @@ export const addNewUserAddressService = async (body: IaddNewUserAddress, userId:
 }
 
 export const getUserAddressService = async (userId: string) => {
-    const address = await prisma.users.findUnique({ where: { id: userId }, include: { addresses: true } });
+    const address = await prisma.users.findUnique({
+        where: { id: userId },
+        select: {
+            addresses: {
+                select: {
+                    id: true,
+                    addressLabel: true,
+                    receiverName: true,
+                    receiverPhoneNumber: true,
+                    addressDisplayName: true,
+                    addressDetails: true,
+                    lat: true,
+                    lon: true,
+                    isDefault: true,
+                    provinceId: true,
+                    province: true,
+                    cityId: true,
+                    city: true,
+                    district: true,
+                    districtId: true,
+                },
+            },
+        },
+    });
+
     if (!address) throw new ApiError(404, "User Address not found");
+
+    const { addresses } = address;
+
+    return addresses;
+};
+
+export const setUserDefaultAddressService = async (addressId: string, userId: string) => {
+    // !Extra validation
+    const address = await prisma.userAddress.findUnique({ where: { id: addressId } });
+    if (!address) throw new ApiError(404, "Address not found");
+
+    // !Set all other address to false first
+    await prisma.userAddress.updateMany({
+        where: { userId },
+        data: { isDefault: false },
+    });
+
+    await prisma.userAddress.update({
+        where: { id: addressId },
+        data: { isDefault: true },
+    });
+
+    // !Return
+    return address;
+}
+
+export const deleteUserAddressService = async (addressId: string, userId: string) => {
+    // !Extra validation
+    const address = await prisma.userAddress.findUnique({ where: { id: addressId } });
+    if (!address) throw new ApiError(404, "Address not found");
+
+    await prisma.userAddress.delete({
+        where: { id: addressId },
+    });
 
     // !Return
     return address;
